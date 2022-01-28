1a200f291a6c3ea10b0664b142a6a51e993885fd
yacn1214 <yacn1214@gmail.com>
2022-01-28 11:55:44 +0330

    new weblog

--- api/dbconfig.php
+++ api/dbconfig.php
@@ -4,6 +4,7 @@ define('DB_NAME','search');
 define('DB_USER','root');
 define('DB_PASS','');
 
+
 try{
     $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
