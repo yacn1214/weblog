ea92b6c21a6f8397a1f5f0f48e708367c77d4c08
yacn1214 <yacn1214@gmail.com>
2022-01-28 12:27:24 +0330

    new weblog

--- admin.php
+++ admin.php
@@ -1,5 +1,5 @@
 <?php
-if($_SERVER['REMOTE_ADDR'] == "46.182.32.2" or $_SERVER['REMOTE_ADDR'] == "5.122.112.150" or $_SERVER['REMOTE_ADDR'] == "127.0.0.1"){
+if(isset($_COOKIE['admin'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,9 +25,9 @@ if($_SERVER['REMOTE_ADDR'] == "46.182.32.2" or $_SERVER['REMOTE_ADDR'] == "5.122
 <div class="alert alert-primary" role="alert">
     <center>
     ادمین <?php
-    if($_SERVER['REMOTE_ADDR'] == "46.182.32.2")echo $admin = "مهدی";
-    if($_SERVER['REMOTE_ADDR'] == "5.122.112.150")echo $admin =  "یاسین";
-    if($_SERVER['REMOTE_ADDR'] == "127.0.0.1")echo $admin =  "لوکال";
+    if($_COOKIE['admin'] == "mahdi")echo $admin = "مهدی";
+    if($_COOKIE['admin'] == "yacn")echo $admin =  "یاسین";
+    if($_COOKIE['admin'] == "local")echo $admin =  "لوکال";
     ?> خوش امدید
         </center>
 </div>
@@ -47,6 +47,136 @@ if($_SERVER['REMOTE_ADDR'] == "46.182.32.2" or $_SERVER['REMOTE_ADDR'] == "5.122
 </body>
 </html>
 <?php }else{
-echo "bache bia paiin";
+?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>login form</title>
+        <style>
+            @import url("https://fonts.googleapis.com/css?family=Raleway:400,700");
+            *, *:before, *:after {
+                box-sizing: border-box;
+            }
+
+            body {
+                min-height: 100vh;
+                font-family: "Raleway", sans-serif;
+            }
+
+            .container {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                overflow: hidden;
+            }
+            .container:hover .top:before, .container:hover .top:after, .container:hover .bottom:before, .container:hover .bottom:after, .container:active .top:before, .container:active .top:after, .container:active .bottom:before, .container:active .bottom:after {
+                margin-left: 200px;
+                transform-origin: -200px 50%;
+                transition-delay: 0s;
+            }
+            .container:hover .center, .container:active .center {
+                opacity: 1;
+                transition-delay: 0.2s;
+            }
+
+            .top:before, .top:after, .bottom:before, .bottom:after {
+                content: "";
+                display: block;
+                position: absolute;
+                width: 200vmax;
+                height: 200vmax;
+                top: 50%;
+                left: 50%;
+                margin-top: -100vmax;
+                transform-origin: 0 50%;
+                transition: all 0.5s cubic-bezier(0.445, 0.05, 0, 1);
+                z-index: 10;
+                opacity: 0.65;
+                transition-delay: 0.2s;
+            }
+
+            .top:before {
+                transform: rotate(45deg);
+                background: #e46569;
+            }
+            .top:after {
+                transform: rotate(135deg);
+                background: #ecaf81;
+            }
+
+            .bottom:before {
+                transform: rotate(-45deg);
+                background: #60b8d4;
+            }
+            .bottom:after {
+                transform: rotate(-135deg);
+                background: #3745b5;
+            }
+
+            .center {
+                position: absolute;
+                width: 400px;
+                height: 400px;
+                top: 50%;
+                left: 50%;
+                margin-left: -200px;
+                margin-top: -200px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                padding: 30px;
+                opacity: 0;
+                transition: all 0.5s cubic-bezier(0.445, 0.05, 0, 1);
+                transition-delay: 0s;
+                color: #333;
+            }
+            .center input {
+                width: 100%;
+                padding: 15px;
+                margin: 5px;
+                border-radius: 1px;
+                border: 1px solid #ccc;
+                font-family: inherit;
+            }
+        </style>
+    </head>
+    <body>
+
+    <div class="container" onclick="onclick">
+        <div class="top"></div>
+        <div class="bottom"></div>
+        <div class="center">
+            <h2>Please Sign In</h2>
+            <form method="post" action="">
+            <input type="username" name="user" placeholder="username"/>
+            <input type="password" name="pass" placeholder="password"/>
+            <input type="submit" name="btnadmin" value="login">
+            </form>
+            <h2>&nbsp;</h2>
+        </div>
+    </div>
+    </body>
+    </html>
+
+<?php
+    if (isset($_POST['btnadmin'])){
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+        if ($user == "yacn" && $pass == "yAcn2020@14"){
+            setcookie("admin",$user,time()+3600);
+            echo "ok";
+            header("location:admin.php");
+        }
+        if ($user == "mahdi" && $pass == "android601"){
+            setcookie("admin",$user,time()+3600);
+            echo "ok";
+            header("location:admin.php");
+        }
+    }
 }
 ?>
